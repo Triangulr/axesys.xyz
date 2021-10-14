9a4f43496711488ff92959b0d6474d053edfe1ee
Axesys <54177105+Triangulr@users.noreply.github.com>
2021-10-14 18:17:21 +0700

    Edit quote

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,8 +66,7 @@ const Home = () => {
 
               <SlideFade in={showDesc} offsetY={-16}>
                 <VStack spacing={8}>
-                  <div>Coding is depressing, but RNG is even more depressing.
-                    Source by phosphene47 (thanks)
+                  <div>The supreme good for man is happiness - perfection of the soul and also body in accordance with virtues.
                   </div>
 
                   <Links />
