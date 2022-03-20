b6adec20056e6cc9098ca08864582706940f3728
Axesys <54177105+Triangulr@users.noreply.github.com>
2022-03-20 10:08:12 +0700

    forgor

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -2,7 +2,7 @@ import { chakra } from "@chakra-ui/react";
 import { memo } from "react";
 
 const Counter = () => {
-  return <chakra.div bg="url(https://count.getloli.com/get/@phos)" />;
+  return <chakra.div bg="url(https://count.getloli.com/get/@axesys)" />;
 };
 
 export default memo(Counter);
